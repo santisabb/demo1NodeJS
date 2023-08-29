f1d5d99e608ed5b1969ffbaf6c30397c6a6787a5
SANTIAGO SABBIONI <santisabbioni@gmail.com>
2023-08-29 17:23:02 -0300

    terminado get y post

--- primer-api/pokemon/routing.js
+++ primer-api/pokemon/routing.js
@@ -1,6 +1,6 @@
 const http = require('node:http')
 
-const ditto = require('ditto.js')
+const ditto = require('./ditto.js')
 
 const processRequest = (req, res) => {
   const { method, url } = req
@@ -9,7 +9,30 @@ const processRequest = (req, res) => {
       switch (url) {
         case '/pokemon/ditto':
           res.setHeader('Content-Type', 'application/json; charset=utf-8')
-          return res.end('puto el que lee')
+          return res.end(JSON.stringify(ditto))
+        default:
+          res.statusCode = 404
+          res.setHeader('Content-Type', 'text/html; charset=utf-8')
+          return res.end('<h1>404 not found bro :,(</h1>')
+      }
+
+    case 'POST':
+      switch (url) {
+        case '/pokemon': {
+          let body = ''
+
+          req.on('data', chunk => {
+            body += chunk
+          })
+
+          req.on('end', () => {
+            const data = JSON.parse(body)
+            res.writeHead(201, { 'Content-Type': 'application/json; charset=utf-8' })
+            res.end(JSON.stringify(data))
+          })
+          break
+        }
+
         default:
           res.statusCode = 404
           res.setHeader('Content-Type', 'text/html; charset=utf-8')
