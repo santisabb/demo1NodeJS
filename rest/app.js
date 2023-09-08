63d2946b94ab303b3bea880269035cfcd038bf34
SANTIAGO SABBIONI <santisabbioni@gmail.com>
2023-09-08 16:34:17 -0300

    agrego directorio middlewares

--- rest/app.js
+++ rest/app.js
@@ -1,6 +1,6 @@
 import express, { json } from 'express'
 import { recordsRouter } from './routes/album.js'
-import { cors } from 'cors'
+import { corsMiddleware } from './middlewares/cors.js'
 
 const app = express()
 const port = process.env.PORT ?? 8081
@@ -8,22 +8,7 @@ const port = process.env.PORT ?? 8081
 app.disable('x-powered-by')
 app.use(json())
 
-app.use(cors({
-  origin: (origin, callback) => {
-    const ACCEPTED_ORIGINS = [
-      'http://localhost:8081'
-    ]
-    if (ACCEPTED_ORIGINS.includes(origin)) {
-      return callback(null, true)
-    }
-
-    if (!origin) {
-      return callback(null, false)
-    }
-
-    return callback(new Error('Not allowed by CORS bro go fuck yourself'))
-  }
-}))
+app.use(corsMiddleware())
 
 app.use('/album', recordsRouter)
 
