dd70478f4ef43ea0bcd0f606e1b70e10f39cc128
SANTIAGO SABBIONI <santisabbioni@gmail.com>
2023-09-08 14:57:59 -0300

    agrego dep cors y  su middleware

--- rest/app.js
+++ rest/app.js
@@ -1,72 +1,31 @@
 import express, { json } from 'express'
-import { createRequire } from 'node:module'
-import { validateAlbum, validatePartialAlbum } from './schemas/album.js'
-const require = createRequire(import.meta.url)
-const rcrds = require('./records.json')
+import { recordsRouter } from './routes/album.js'
+import { cors } from 'cors'
+
 const app = express()
 const port = process.env.PORT ?? 8081
 
 app.disable('x-powered-by')
 app.use(json())
 
-app.get('/records', (req, res) => {
-  const { artist } = req.query
-  if (artist) {
-    const filteredRec = rcrds.filter(
-      record => record.artist.some(n => n.toLowerCase() === artist.toLowerCase())
-    )
-    return res.json(filteredRec)
+app.use(cors({
+  origin: (origin, callback) => {
+    const ACCEPTED_ORIGINS = [
+      'http://localhost:8081'
+    ]
+    if (ACCEPTED_ORIGINS.includes(origin)) {
+      return callback(null, true)
+    }
+
+    if (!origin) {
+      return callback(null, false)
+    }
+
+    return callback(new Error('Not allowed by CORS bro go fuck yourself'))
   }
-  res.json(rcrds)
-})
+}))
 
-app.get('/records/:name', (req, res) => {
-  const { name } = req.params
-  const record = rcrds.find(record => record.name === name)
-  if (record) res.json(record)
-
-  res.status(404).json({ message: 'Not found that name bro' })
-})
-
-app.post('/records', (req, res) => {
-  const result = validateAlbum(req.body)
-
-  if (!result.success) {
-    return res.status(400).json({ error: JSON.parse(result.error.message) })
-  }
-
-  const newAlbum = {
-    ...result.data
-  }
-
-  rcrds.push(newAlbum)
-
-  res.status(201).json(newAlbum)
-})
-
-app.patch('/records/:name', (req, res) => {
-  const result = validatePartialAlbum(req.body)
-
-  if (!result.success) {
-    return res.status(400).json({ error: JSON.parse(result.error.message) })
-  }
-
-  const { name } = req.params
-  const albumIndex = rcrds.findIndex(a => a.name === name)
-
-  if (albumIndex === -1) {
-    return res.status(404).json({ message: 'Album not found :(' })
-  }
-
-  const updateRecord = {
-    ...rcrds[albumIndex],
-    ...result.data
-  }
-
-  rcrds[albumIndex] = updateRecord
-
-  return res.json(updateRecord)
-})
+app.use('/album', recordsRouter)
 
 app.listen(port, () => {
   console.log('server listening on port http://localhost:8081')
